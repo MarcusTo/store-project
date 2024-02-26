2c52a8b7a8036a081a5fd470073338124e8554c6
Marcus Toman <marcus.toman@flowit.ee>
2024-02-26 11:46:44 +0200

    Refactor code, navbar styling in-progress.

--- store-project/src/router/routes.ts
+++ store-project/src/router/routes.ts
@@ -29,7 +29,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: ROUTE_PATHS.PRODUCTS,
     name: ROUTE_PATHS.PRODUCTS,
-    component: () => import("@/views/Products.vue"),
+    component: () => import("@/views/Product.vue"),
     meta: {
       breadcrumb: [{ label: "Products", route: "/products" }],
     },
