0358a410eafa244888ea1b51f0cd4741785d4a62
Marcus Toman <marcus.toman@flowit.ee>
2024-02-27 12:13:37 +0200

    feature: Add service card to Home page

--- store-project/src/views/Home.vue
+++ store-project/src/views/Home.vue
@@ -4,14 +4,15 @@
   </ProductCardComp>
   <CardComp> 
   </CardComp>
-
+  <ServiceCardComp>
+  </ServiceCardComp>
  
 </template>
 
 <script setup lang="ts">
 import NavBarComp from "@/components/NavBarComp.vue";
 import CardComp from "@/components/CardComp.vue";
-import ServiceCardComp from "@/components/CardComp.vue";
+import ServiceCardComp from "@/components/ServiceCardComp.vue";
 import ProductCardComp from "@/components/ProductCardComp.vue";
 </script>
 
