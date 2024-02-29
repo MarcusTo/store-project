8938af96d777d7bdef25a60aa9e70347da5db34e
Marcus Toman <marcus.toman@flowit.ee>
2024-02-29 13:35:39 +0200

    feat/ref: Add searchbar to producst view, change styling

---
+++ store-project/src/components/SearchComp.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="search">
+    <div class="input-group">
+      <input
+        class="text"
+        v-model="searchText"
+        type="text"
+        placeholder="Mudel"
+        @input="handleInput"
+      />
+      <i class="pi pi-search"></i>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+let searchText = ref("");
+
+const handleInput = () => {};
+</script>
+
+<style scoped>
+.search {
+  align-items: center;
+  justify-content: center;
+  display: flex;
+}
+
+.input-group {
+  position: relative;
+  width: 300px; 
+  align-items: center;
+  justify-content: center;
+  display: flex;
+}
+
+.text {
+  padding-left: 35px; 
+  height: 40px; 
+  width: 50%;
+  font-size: 16px; 
+
+}
+
+.pi-search {
+  position: absolute;
+  top: 10px; 
+  left: 10px; 
+  margin-left: 54px;
+  font-size: 20px; 
+}
+</style>
--- store-project/src/views/Product.vue
+++ store-project/src/views/Product.vue
@@ -1,12 +1,13 @@
 <template>
   <NavBarComp />
-    
+  <SearchComp/>
   <FooterComp />
 </template>
 
 <script setup lang="ts">
 import FooterComp from "@/components/FooterComp.vue";
 import NavBarComp from "@/components/NavBarComp.vue";
+import SearchComp from "@/components/SearchComp.vue";
 </script>
 
 <style scoped></style>
