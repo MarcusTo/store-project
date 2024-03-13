1bbbb45619e7551d1ff84a3fe02bf90398ff175e
Marcus Toman <toman.markus12@gmail.com>
2024-03-13 14:58:16 +0200

    test

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -12,9 +12,13 @@
       {{ t("cart.title") }}
     </h2>
     <div v-for="(item, index) in cart.cartItems" :key="index">
-      <p>{{ item.name }}</p>
-      <!-- ...display other properties of the item -->
+      <h2>{{ item.name }}</h2>
+      <p>{{ item.selectedMemory }}</p>
+      <p>{{ item.price }}</p>
     </div>
+    <div>
+    <h2>Total Price: {{ totalPrice }}</h2>
+  </div>
 
     <div class="empty-cart" v-if="cart.cartItems.length === 0">
       <img src="/img/cart_placeholder.svg" />
@@ -24,15 +28,18 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed } from "vue";
 import { useCartStore } from '@/stores/cart'
 import NavBarComp from "@/components/NavBarComp.vue";
 import FooterComp from "@/components/FooterComp.vue";
 import { useI18n } from "vue-i18n";
 
 const cart = useCartStore()
-const cartItems = ref([]);
 const { t } = useI18n();
+
+const totalPrice = computed(() => {
+  return cart.cartItems.reduce((total, item) => total + item.price, 0)
+})
 </script>
 
 <style scoped>
